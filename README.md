551c45b4c4a0a7b650724f62dab21fa63a7f83ed
Joey Bronner <joeybronner@gmail.com>
2016-05-03 13:10:16 +0200

    Source code

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# temperature-sensor-8x8matrix
-This prototype controls an 8x8 LED matrix following a temperature sensor value in input.
+# Temperature Sensor 8x8 Matrix
+Arduino 8x8 matrix controlled by temperature sensor
+
+## Full article
+No blog article for the moment  
+
+## Prototype
+![Prototype](http://blog.joeybronner.fr/wp-content/uploads/2016/03/IMG_20160327_230141-2.jpg)  
+
+## Screen connection
+![Prototype](http://blog.joeybronner.fr/wp-content/uploads/2016/03/screen_4digits_7segments_connection_SH5461AS.png)    
---
+++ temp/temp.ino
@@ -0,0 +1,177 @@
+/*
+ *  -------- TEMPERATE SENSOR 8x8 MATRIX --------
+ *  
+ *  This prototype controls an 8x8 LED matrix following a temperature sensor 
+ *  value in input.
+ *  
+ *  ---------
+ *  Created 01 May 2016
+ *  by Joey Bronner
+ *  ---------
+ *  
+ *  Pin numbers:
+ *  Matrix:
+ *  Digital pins 2 through 13
+ *  Analog pins 2 through 5 used as digital 16 through 19
+ *  
+ *  Souce code public and open, hosted on @Github:
+ *  http://github.com/joeybronner/temperature-sensor-8x8matrix
+ *  
+ *  See also http://blog.joeybronner.fr for more projects, infos, etc.
+ */
+
+// 2-dimensional array of row/column pin numbers
+const int row[8] = {
+  2, 7, 19, 5, 13, 18, 12, 16
+};
+const int col[8] = {
+  6, 11, 10, 3, 17, 4, 8, 9
+};
+
+// 2-dimensional array of pixels
+int pixels[8][8];
+
+// cursor position
+int x = 5;
+int y = 5;
+
+// sensor analog pin
+int sensor = analogRead(0);
+
+void setup() {  
+  // initialize the I/O pins as outputs : iterate over the pins and initialize it
+  for (int thisPin = 0; thisPin < 8; thisPin++) {
+    pinMode(col[thisPin], OUTPUT);
+    pinMode(row[thisPin], OUTPUT);
+    // take the col pins (i.e. the cathodes) high to ensure that the LEDS are off
+    digitalWrite(col[thisPin], HIGH);
+  }
+
+  // initialize the pixel matrix
+  for (int x = 0; x < 8; x++) {
+    for (int y = 0; y < 8; y++) {
+      pixels[x][y] = HIGH;
+    }
+  }
+}
+
+void loop() {
+  float mv = (sensor/1024.0)*5000; 
+  float temperatureC = mv/10;
+
+  clean();
+  if (temperatureC < 19) {
+    sad();
+  } else if (temperatureC >= 19 && temperatureC < 23) {
+    smile();
+  } else if (temperatureC >= 23) {
+    sad();
+  } else {
+    whathappens();
+  }
+  
+  // draw the screen
+  refreshScreen();
+}
+
+void refreshScreen() {
+  // iterate over the rows (anodes)
+  for (int thisRow = 0; thisRow < 8; thisRow++) {
+    // take the row pin (anode) high
+    digitalWrite(row[thisRow], HIGH);
+    // iterate over the cols (cathodes)
+    for (int thisCol = 0; thisCol < 8; thisCol++) {
+      // get the state of the current pixel
+      int thisPixel = pixels[thisRow][thisCol];
+      // when the row is HIGH and the col is LOW the LED where they meet turns on:
+      digitalWrite(col[thisCol], thisPixel);
+      // turn the pixel off
+      if (thisPixel == LOW) {
+        digitalWrite(col[thisCol], HIGH);
+      }
+    }
+    // take the row pin low to turn off the whole row:
+    digitalWrite(row[thisRow], LOW);
+  }
+}
+
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+// [ ][ ][o][o][o][o][ ][ ]
+// [ ][o][ ][ ][ ][ ][o][ ]
+// [ ][ ][ ][ ][ ][ ][o][ ]
+// [ ][ ][ ][ ][o][o][ ][ ]
+// [ ][ ][ ][ ][o][ ][ ][ ]
+// [ ][ ][ ][ ][o][ ][ ][ ]
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+// [ ][ ][ ][ ][o][ ][ ][ ]
+void whathappens() {
+  
+}
+
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+// [ ][ ][o][ ][ ][o][ ][ ]
+// [ ][ ][o][ ][ ][o][ ][ ]
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+// [ ][o][ ][ ][ ][ ][o][ ]
+// [ ][ ][o][o][o][o][ ][ ]
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+void smile() {
+  eyes();
+
+  // mouth
+  pixels[5][1] = LOW;
+  pixels[6][2] = LOW;
+  pixels[6][3] = LOW;
+  pixels[6][4] = LOW;
+  pixels[6][5] = LOW;
+  pixels[5][6] = LOW;
+}
+
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+// [ ][ ][o][ ][ ][o][ ][ ]
+// [ ][ ][o][ ][ ][o][ ][ ]
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+// [ ][ ][o][o][o][o][ ][ ]
+// [ ][o][ ][ ][ ][ ][o][ ]
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+void sad() {
+  eyes();
+
+  // mouth
+  pixels[6][1] = LOW;
+  pixels[5][2] = LOW;
+  pixels[5][3] = LOW;
+  pixels[5][4] = LOW;
+  pixels[5][5] = LOW;
+  pixels[6][6] = LOW;
+}
+
+void eyes() {
+  pixels[1][2] = LOW;
+  pixels[2][2] = LOW;
+  pixels[1][5] = LOW;
+  pixels[2][5] = LOW;
+}
+
+void nose() {
+  pixels[3][3] = LOW;
+  pixels[3][4] = LOW;
+}
+
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+void clean() {
+  for (int x = 0; x < 8; x++) {
+    for (int y = 0; y < 8; y++) {
+      pixels[x][y] = HIGH;
+    }
+  }
+}
